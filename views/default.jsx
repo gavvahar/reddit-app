185ee11fe80cb8e01e9da0b12914e2a433818da1
Bachir <bachirmdicko@gmail.com>
2022-10-22 11:00:07 -0400

    commit

--- views/default.jsx
+++ views/default.jsx
@@ -5,7 +5,7 @@ function Def(html){
             <head>
                 <title>Home Page</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"/>
-				<link rel="stylesheet" href="/css/style.css"/>
+                <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
                 <nav>
@@ -14,10 +14,10 @@ function Def(html){
                             <a href="/">Home</a>
                         </li>
                         <li>
-                            <a href="/places">Places</a>
+                            <a href="/places">Posts</a>
                         </li>
                         <li>
-                            <a href="/places/new">Add Place</a>
+                            <a href="/places/new">Add Post</a>
                         </li>
                     </ul>
                 </nav>
