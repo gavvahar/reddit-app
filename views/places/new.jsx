7ca195b73b5839ccbdbaf79bb93829f52dc1615e
unknown <cabaraul@gmail.com>
2022-10-27 20:13:30 -0400

    added textarea to edit form

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -22,21 +22,13 @@ function new_form (data) {
               
                 <div className="form-group">
                     <label htmlFor="description">Description</label>
-                    <input className="form-control" id="description" name="description" />
+                    <textarea className="form-control" id="description" name="description" rows="3"></textarea>
                 </div>
                   <div className="form-group">
                     <label htmlFor="pic">Place Picture</label>
                     <input className="form-control" id="pic" name="pic" />
                 </div>
-                <div className="form-group col-sm-4">
-                    <label htmlFor="founded">Founded Year</label>
-                    <input 
-                        type="number"
-                        className="form-control"
-                        id="founded"
-                        name="founded" 
-                        value={new Date().getFullYear()} />
-                    </div>
+                
                 <input className="btn btn-primary" type="submit" value="Add Post" />
             </form>
           </main>
