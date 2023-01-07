2d05484fde7cddf22d4711bd94d6a28ed51d0893
gavvahar <nihargavva@gmail.com>
2023-01-07 12:33:41 -0500

    updating path to use scss files

--- views/places/default.jsx
+++ views/places/default.jsx
@@ -6,7 +6,7 @@ function Def(html){
             <head>
                 <title>Home Page</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"/>
-				<link rel="stylesheet" href="/css/style.css"/>
+				<link rel="stylesheet" href="/scss/style.css"/>
             </head>
             <body>
                 <nav>
