54181427f7668064574168496cf0a479473c31d1
unknown <cabaraul@gmail.com>
2023-01-20 23:27:24 -0500

    bachir

--- views/places/default.jsx
+++ views/places/default.jsx
@@ -14,7 +14,6 @@ function Def(html){
                         <input  type='text' placeholder='Search For a Post' name='title' />
                         <button className="btn btn-success">Search</button>
                     </form>
-                    </div>
                     <ul>
                         <li>
                             <a href="/">Home</a>
@@ -26,56 +25,8 @@ function Def(html){
                             <a href="/places/new">Add Post</a>
                         </li>
                     </ul>
-                    <div className="container">
-        <form className="form" id="login" method='POST' action='/api/signin'>
-            <h1 className="form__title">Login</h1>
-            <div className="form__message form__message--error"></div>
-            <div className="form__input-group">
-                <input type="text" className="form__input" autofocus placeholder="Username or email" id="email" name="email"/>
-                <div className="form__input-error-message"></div>
-            </div>
-            <div className="form__input-group">
-                <input type="password" className="form__input" autofocus placeholder="Password" id="password" name="password" />
-                <div className="form__input-error-message"></div>
-            </div>
-            <button className="form__button" type="submit">Continue</button>
-            <p className="form__text">
-                <a href="#" className="form__link">Forgot your password?</a>
-            </p>
-            <p className="form__text">
-                <a className="form__link" href="places/createAccount" id="linkCreateAccount">Don't have an account? Create account</a>
-            </p>
-        </form>
-        <form className="form form--hidden" id="createAccount">
-            <h1 className="form__title">Create Account</h1>
-            <div className="form__message form__message--error"></div>
-            <div className="form__input-group">
-                <input type="text" id="signupUsername" className="form__input" autofocus placeholder="Username" name="name"/>
-                <div className="form__input-error-message"></div>
-            </div>
-            <div className="form__input-group">
-                <input type="text" className="form__input" autofocus placeholder="Email Address" name="email" />
-                <div className="form__input-error-message"></div>
-            </div>
-            <div className="form__input-group">
-                <input type="password" className="form__input" autofocus placeholder="Password" name="password"/>
-                <div className="form__input-error-message"></div>
-            </div>
-            <div className="form__input-group">
-                <input type="password" className="form__input" autofocus placeholder="Confirm password" />
-                <div className="form__input-error-message"></div>
-            </div>
-            <button className="form__button" type="submit">Continue</button>
-            <p className="form__text">
-                <a className="form__link" href="./" id="linkLogin">Already have an account? Sign in</a>
-            </p>
-        </form>
-    </div>
-    
                 </nav>
-                
                 {html.children}
-                
             </body>
         </html>
     )
